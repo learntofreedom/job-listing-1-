82198e022e7f9c15930b90df84360215b99426f5
learntofreedom <learntofeedom@gmail.com>
2017-02-25 13:26:40 +0800

    step 7 upload resume

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :jobs
-   root 'jobs#index'
-    namespace :admin do
+
+  namespace :admin do
     resources :jobs do
       member do
         post :publish
@@ -10,4 +9,9 @@ Rails.application.routes.draw do
       end
     end
   end
+  resources :jobs do
+  resources :resumes
+end
+
+  root 'jobs#index'
 end
